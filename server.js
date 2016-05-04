4178b941dc8403624b9ab9412f46c55392974bf0
Michael Brewer <mikeb@steelhouse.com>
2016-05-04 12:23:31 -0700

    added simple static server

---
+++ server.js
@@ -0,0 +1,21 @@
+var port = process.env.PORT || 3000;
+var http = require('http');
+var url  = require('url');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static')("./");
+
+var server = http.createServer(function(req, res) {
+	console.log(req.url);
+
+	var done = finalhandler(req, res);
+	var urlParts = url.parse(req.url);
+
+	switch (urlParts.path) {
+		default:
+			serveStatic(req, res, done);
+	}
+})
+.listen(port);
+
+console.log('listening on port', port);
